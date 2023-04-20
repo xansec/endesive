d27a8a5028f388361bc24f412370b012374ab46c
jake <jake@MayhemVM.myguest.virtualbox.org>
2023-04-19 23:22:54 -0400

    Update fuzzer

--- mayhem/fuzz_verify.py
+++ mayhem/fuzz_verify.py
@@ -2,50 +2,53 @@
 
 import atheris
 import sys
-#sys.path.append('.')
+import datetime
+from cryptography.hazmat import backends
+from cryptography.hazmat.primitives.serialization import pkcs12
 
-with atheris.instrument_imports():
-	from endesive.pdf.fpdf import FPDF
-	from PyPDF2 import PdfReader, PdfWriter
+#with atheris.instrument_imports():
+from endesive import pdf
 
 
 
-
-@atheris.instrument_func
+#@atheris.instrument_func
 def fuzz_test_verify(input_data):
     fdp = atheris.FuzzedDataProvider(input_data)
-    result = fdp.ConsumeRegularFloat()
-   
+    result = fdp.ConsumeBytes(fdp.remaining_bytes())
+    if result == b'':
+        return 0
 
-    # Call the verify function with the input data
     try:
-        doc = FPDF()
-        doc.pdf_version = "1.3"
-        doc.set_compression(0)
-        font = 'helvetica'
-        doc.add_page()
-        doc.set_font(font, '', 13.0)
-        doc.cell(w=result, h=result, align='C', txt="Test", border=result, ln=2, link=None)   
-        doc.output("Outputfile", "F")
-        
-        fname = "Outputfile"
-        with open(fname, "rb") as in_file:
-            input_pdf = PdfReader(in_file)
-            output_pdf = PdfWriter()
-            output_pdf.encrypt("1234", "1234")
-            with open("Outputfile-encrypted", "wb") as out_file:        
-                output_pdf.write(out_file)
-            
-        
-    except Exception as e:
+        dct = {
+            'sigflags': 3,
+            'contact': 'jake@mayhem.com',
+            'location': 'Elsewhere',
+            'signingdate': datetime.date.today,
+            'reason': 'For Mayhem',
+        }
+        with open('output-file', 'wb') as file:
+            file.write(result)
+        file.close()
+        with open('output-file', 'rb') as file:
+            f = file.read()
+            pdf.cms.sign(f,
+                         dct,
+                         p12[0],
+                         p12[1],
+                         p12[2],
+                         'sha256')
+        file.close()
+    except ValueError:
         return -1
-
 
 
 def main():
     atheris.Setup(sys.argv, fuzz_test_verify)
     atheris.Fuzz()
 
-if __name__ == "__main__":
-    main()
 
+if __name__ == "__main__":
+    with open('demo2_user1.p12', 'rb') as fp:
+        p12 = pkcs12.load_key_and_certificates(fp.read(), b'1234', backends.default_backend())
+
+    main()
